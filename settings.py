97db5f5145f4384f2944002d50482334dc5ace83
Evan Broder <broder@mit.edu>
2010-01-11 01:01:37 -0500

    Revert "We don't need the settings module."
    This reverts commit d1d425068e9e07b70118468b0c2ead7f28bc9ff7.

--- settings.py
+++ settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.admindocs',
     'tagging',
