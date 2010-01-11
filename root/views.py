bd4389d357dcf3428d6f05912905d662f2c24a9b
Evan Broder <broder@mit.edu>
2010-01-10 23:48:42 -0500

    Add a simple display view (no template yet)

--- root/views.py
+++ root/views.py
@@ -5,3 +5,7 @@ from ibtsocs.root.models import Post
 def index(request):
     posts = Post.objects.order_by('-created')[:10]
     return render_to_response('root/index.html', {'posts': posts})
+
+def display(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render_to_response('root/display.html', {'post': post})
