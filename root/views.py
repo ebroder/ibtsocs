9d80e4571e88e7344b21f1fa618b68f3ed379695
Evan Broder <broder@mit.edu>
2010-01-10 23:44:56 -0500

    Setup a /very/ basic home page view and template.

--- root/views.py
+++ root/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+
+from ibtsocs.root.models import Post
+
+def index(request):
+    posts = Post.objects.order_by('-created')[:10]
+    return render_to_response('root/index.html', {'posts': posts})
