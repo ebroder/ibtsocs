1f39992f1418ec19f2bf0cddc0ee55ad9747cd86
Evan Broder <broder@mit.edu>
2010-01-10 22:26:56 -0500

    Expose posts in the admin interface.

---
+++ root/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.forms import widgets
+
+from ibtsocs.root.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['message', 'nick']
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'message':
+            kwargs['widget'] = widgets.Textarea
+        return super(PostAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+admin.site.register(Post, PostAdmin)
