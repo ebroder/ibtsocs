bf121847d7b588030be8bcc8db594b0494ad0a48
Evan Broder <broder@mit.edu>
2010-01-11 02:30:42 -0500

    Add redirect from /posts to /

--- urls.py
+++ urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^posts/$', 'django.views.generic.simple.redirect_to', {'url': '/'}),
 )
 
 urlpatterns += patterns(
