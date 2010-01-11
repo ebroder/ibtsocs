a9366cf3b0a67a5465d1708362fa0430a1821b85
Evan Broder <broder@mit.edu>
2010-01-11 02:23:48 -0500

    Make voting work.

---
+++ root/templatetags/post_extras.py
@@ -0,0 +1,14 @@
+from django import template
+from django.core.urlresolvers import reverse
+from django.utils.html import conditional_escape
+
+register = template.Library()
+
+@register.simple_tag
+def link_if_not_voted(post, visitor, vote_dir, text):
+    text = conditional_escape(text)
+    if not post.voted(visitor):
+        url = reverse('ibtsocs.root.views.vote', args=(post.id, vote_dir))
+        return '<a href="%s">%s</a>' % (url, text)
+    else:
+        return text
--- root/views.py
+++ root/views.py
@@ -4,11 +4,15 @@ from ibtsocs.root.models import Post, Vote
 
 def index(request):
     posts = Post.objects.order_by('-created')[:10]
-    return render_to_response('root/index.html', {'posts': posts})
+    return render_to_response('root/index.html',
+                              {'posts': posts,
+                               'visitor': request.session.session_key})
 
 def display(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render_to_response('root/display.html', {'post': post})
+    return render_to_response('root/display.html',
+                              {'post': post,
+                               'visitor': request.session.session_key})
 
 def vote(request, post_id, direction):
     post = get_object_or_404(Post, pk=post_id)
