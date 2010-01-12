a50e65eec2d62cf3e9c46d70212b7e65034b2b8c
Evan Broder <broder@mit.edu>
2010-01-11 23:29:36 -0500

    Add some convenience redirects.

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns(
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^posts/$', 'django.views.generic.simple.redirect_to', {'url': '/'}),
+    (r'^feeds?/$', 'django.views.generic.simple.redirect_to', {'url': '/feeds/posts'}),
     (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 )
 
